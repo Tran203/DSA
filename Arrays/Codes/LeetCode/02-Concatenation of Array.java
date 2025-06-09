2456816ed1df2bd1565f1371302d677036047458
Tran203 <78562630+Tran203@users.noreply.github.com>
2025-06-09 21:56:15 +0200

    Update 02-Concatenation of Array.java

--- Arrays/Codes/LeetCode/02-Concatenation of Array.java
+++ Arrays/Codes/LeetCode/02-Concatenation of Array.java
@@ -5,10 +5,13 @@ class Solution {
         int[] ans = new int[2*nums.length];
 
         for(int i = 0; i < nums.length; i++){
-            ans[i] = nums[i];
-            ans[i + nums.length] = nums[i];
+            ans[i] = nums[i];// copy to current
+            ans[i + nums.length] = nums[i]; // copy to the second half(second array)
         }
 
         return ans;
     }
 }
+
+//Trick of the Trade
+//1.need to output array that’s twice the size of the input
